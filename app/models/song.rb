391dfde2a71ca6e938c6bfd55a2d7c081b6cd81e
Tyler Hunt <tyhunt04@gmail.com>
2019-05-02 17:52:50 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,20 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def note_contents=(contents)
+    contents.each do |content|
+      if content != ""
+        note = Note.find_or_create_by(content: content)
+        self.notes << note
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map {|note| note.content}
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
 end
